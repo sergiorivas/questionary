5f800849577ac6bea7c2e8ac163a0397ad03396e
Sergio Rivas <sergiorivas@gmail.com>
2025-04-20 16:04:48 -0400

    chore: 🌐 enable ngrok for testing

--- interview-app/vite.config.ts
+++ interview-app/vite.config.ts
@@ -6,6 +6,12 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    allowedHosts: [
+      'localhost',
+      '.ngrok-free.app', // Esto permitirá cualquier subdominio de ngrok-free.app
+    ]
+  },
   test: {
     globals: true,
     environment: 'happy-dom',
